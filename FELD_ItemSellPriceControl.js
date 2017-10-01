46d988b48b271145dcd719d5da3baa1ff3c67b34
Feldherren <rpaliwoda@googlemail.com>
2017-10-01 15:11:54 +0100

    Fixed formatting in help text within plugin. Revised README.md text slightly.

--- FELD_ItemSellPriceControl.js
+++ FELD_ItemSellPriceControl.js
@@ -12,19 +12,18 @@
  *
  * @help Item Sell Price Control v1.0, by Feldherren (rpaliwoda AT googlemail.com)
  
-Allows you to set whether item selling price is calculated based on the database
-price, or the shop price; by default RPG Maker MV only has the selling price 
-dependent on the database price, even if the shop has a custom price for the item
-in question.
-Allows you to change the sell-price multiplier on initialisation (as a plugin option), 
-and mid-game (via plugin command).
+Allows you to set whether item selling price is calculated based on the 
+database price, or the shop price; by default RPG Maker MV only has the 
+selling price dependent on the database price, even if the shop has a 
+custom price for the item in question.
 
-Allows you to change item prices mid-game via plugin command or script call?
+Additionally, allows you to change the sell-price multiplier from game
+start (as a plugin parameter), and mid-game (via plugin command).
 
 Plugin commands:
 SETSELLPRICEDEPENDENCY [shop|database]
-Changes whether the sell price is dependent on the shop value, or the database value,
-of an item.
+Changes whether the sell price is dependent on the shop value, or the 
+database value, of an item.
 SELLINGMULTIPLIER [float]
 Changes the sell-price multiplier to the specified float.
 
