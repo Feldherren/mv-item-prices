493826dd52f364e4ae61fc7f1fca39f05355493c
Feldherren <rpaliwoda@googlemail.com>
2017-10-01 20:40:42 +0100

    Now lets you set buy and sell prices for specific objects.

--- FELD_ItemSellPriceControl.js
+++ FELD_ItemSellPriceControl.js
@@ -26,6 +26,12 @@ Changes whether the sell price is dependent on the shop value, or the
 database value, of an item.
 SELLINGMULTIPLIER [float]
 Changes the sell-price multiplier to the specified float.
+SETSELLPRICE [item|weapon|armor] [id] [price]
+Sets the selling price for the item/weapon/armor with the specified ID to the given price.
+UNSETSELLPRICE [item|weapon|armor] [id]
+SETBUYPRICE [item|weapon|armor] [id] [price]
+Sets the buying price for the item/weapon/armor with the specified ID to the given price.
+UNSETBUYPRICE [item|weapon|armor] [id]
 
 Free for use with commercial projects, though I'd appreciate being
 contacted if you do use it in any games, just to know.
@@ -35,20 +41,87 @@ contacted if you do use it in any games, just to know.
 	
 	var shopSellPriceDependentOnShopBuyPrice = (parameters["Item Sell Price Dependent on Shop Price"] == 'shop');;
 	var sellMultiplier = parseFloat(parameters["Item Sell Price Multiplier"]);
+	
+	var itemBuyPrices = new Array();
+	var weaponBuyPrices = new Array();
+	var armorBuyPrices = new Array();
+	
+	var itemSellPrices = new Array();
+	var weaponSellPrices = new Array();
+	var armorSellPrices = new Array();
 
+	// new sellingPrice function
 	var oldSellingPrice = Scene_Shop.prototype.sellingPrice;
 	Scene_Shop.prototype.sellingPrice = function() {
 		//Calls the old method.
-		oldSellingPrice.call(this);
+		var sellingPrice = oldSellingPrice.call(this);
 		// check that shopSellPriceDependentOnShopBuyPrice is true
 		if (shopSellPriceDependentOnShopBuyPrice) // dependent on shop value
 		{
-			return Math.floor(this._buyWindow.price(this._item) * sellMultiplier);
+			sellingPrice = Math.floor(this._buyWindow.price(this._item) * sellMultiplier);
 		}
 		else // dependent on database value
 		{
-			return Math.floor(this._item.price * sellMultiplier);
+			sellingPrice = Math.floor(this._item.price * sellMultiplier);
 		}
+		
+		if (DataManager.isItem(this._item)/* && this._item.itypeId === 1*/) // item
+		{
+			if (this._item.id in itemSellPrices)
+			{
+				sellingPrice = itemSellPrices[this._item.id];
+			}
+		}
+		else if (DataManager.isWeapon(this._item)) // weapon
+		{
+			if (this._item.id in weaponSellPrices)
+			{
+				sellingPrice = weaponSellPrices[this._item.id];
+			}
+		}
+		else if (DataManager.isArmor(this._item)) // armor
+		{
+			if (this._item.id in armorSellPrices)
+			{
+				sellingPrice = armorSellPrices[this._item.id];
+			}
+		}
+		// else if (DataManager.isItem(this._item) && this._item.itypeId === 2) // key item; may not actually need this?
+		// {
+		// }
+		
+		return sellingPrice;
+	};
+	
+	// new buyingPrice function
+	var oldBuyingPrice = Scene_Shop.prototype.buyingPrice;
+	Scene_Shop.prototype.buyingPrice = function() {
+		buyingPrice = oldBuyingPrice.call(this);
+		//console.log(this._item)
+		//console.log(DataManager.isWeapon(this._item));
+		if (DataManager.isItem(this._item)/* && this._item.itypeId === 1*/) // item
+		{
+			if (this._item.id in itemBuyPrices)
+			{
+				buyingPrice = itemBuyPrices[this._item.id];
+			}
+		}
+		else if (DataManager.isWeapon(this._item)) // weapon
+		{
+			if (this._item.id in weaponBuyPrices)
+			{
+				buyingPrice = weaponBuyPrices[this._item.id];
+			}
+		}
+		else if (DataManager.isArmor(this._item)) // armor
+		{
+			if (this._item.id in armorBuyPrices)
+			{
+				buyingPrice = armorBuyPrices[this._item.id];
+			}
+		}
+		
+		return buyingPrice;
 	};
 
 	var Feld_InventoryValue_aliasPluginCommand = Game_Interpreter.prototype.pluginCommand;
@@ -71,6 +144,36 @@ contacted if you do use it in any games, just to know.
 		else if(command == "SELLINGMULTIPLIER" && args[0] != null)
 		{
 			sellMultiplier = parseFloat(args[0]);
+		}
+		else if(command == "SETSELLPRICE" && args[0] != null && args[1] != null && args[2] != null)
+		{
+			if (args[0] == 'item')
+			{
+				itemSellPrices[parseInt(args[1])] = parseInt(args[2]);
+			}
+			else if (args[0] == 'weapon')
+			{
+				weaponSellPrices[parseInt(args[1])] = parseInt(args[2]);
+			}
+			else if (args[0] == 'armor')
+			{
+				armorSellPrices[parseInt(args[1])] = parseInt(args[2]);
+			}
+		}
+		else if(command == "SETBUYPRICE" && args[0] != null && args[1] != null && args[2] != null)
+		{
+			if (args[0] == 'item')
+			{
+				itemBuyPrices[parseInt(args[1])] = parseInt(args[2]);
+			}
+			else if (args[0] == 'weapon')
+			{
+				weaponBuyPrices[parseInt(args[1])] = parseInt(args[2]);
+			}
+			else if (args[0] == 'armor')
+			{
+				armorBuyPrices[parseInt(args[1])] = parseInt(args[2]);
+			}
 		}
 	}
 })();
